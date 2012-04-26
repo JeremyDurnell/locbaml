5a02e8864ea4d854ce3e603af0a7780f484a8227
Jeremy Durnell <jeremy@durnell.me>
2012-04-25 20:38:21 -0500

    initial check-in

---
+++ ClickToBuild.bat
@@ -0,0 +1,4 @@
+"%windir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" locbaml.sln /p:Configuration=Release
+
+rd obj /s /q
+pause
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+// download source from microsoft: http://archive.msdn.microsoft.com/wpfsamples - LocBaml Tool Sample
+// direct download link: http://go.microsoft.com/fwlink/?LinkID=160016
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyKeyName("")]
